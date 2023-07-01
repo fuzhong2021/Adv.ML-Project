8eb50fd44e67b0795652c54da80bc35b0fa25c4e
fuzhong2020 <62815355+fuzhong2020@users.noreply.github.com>
2023-07-01 17:17:55 +0200

    update analysis file

--- scripts/cleaner.py
+++ scripts/cleaner.py
@@ -13,8 +13,6 @@ def load_and_preprocess_data(filename):
     # Lade den Datensatz
     df = pd.read_csv(filename, delimiter='\t', encoding='utf-8')
 
-
-
     # Prüfe, ob es fehlende Werte gibt
     if df.isnull().values.any():
         print("Es gibt fehlende Werte im Datensatz. Diese werden entfernt.")
@@ -32,20 +30,34 @@ def load_and_preprocess_data(filename):
     if 'Date' in df.columns:
         df = df.drop(columns=['Date'])
 
-    # Entfernen aller nicht Englische oder Deutsche Texte
-    df = df[df['Language'].isin(['English', 'German'])]
+    # Entfernen aller nicht Englische, Deutsche oder Französische Texte
+    df = df[df['Language'].isin(['English', 'German', 'French'])]
 
     unique_sources = df['Source'].unique()
     print(unique_sources)
 
     # Entfernen von Stoppwörtern und Durchführen von Stemming
-    stop_words = set(stopwords.words(['german', 'english']))
-    stemmer = SnowballStemmer('english') # Ersetzen Sie 'english' durch 'german' für deutsche Texte
+    stop_words = set(stopwords.words(['german', 'english', 'french']))
+    stemmer = SnowballStemmer('english') # Ersetzen Sie 'english' durch 'german' oder 'french' für deutsche oder französische Texte
     df['Text'] = df['Text'].apply(lambda x: ' '.join(stemmer.stem(word) for word in x.split() if word not in stop_words))
 
-    # Erstellen Zielspalte für Populismus basierend auf der Quelle des Artikels
-    df['Populism'] = df['Source'].apply(lambda x: 1 if x == 'Bild' or x == 'nypostonline.com' else 0)
+    # Gewichtete Liste von populistischen Begriffen pro Sprache
+    populist_terms = {
+        'English': {'elite': 1.5, 'mainstream media': 2, 'people': 1, 'corrupt': 1.5, 'brothers sisters': 1, 'islamic world': 1, 'dear friends': 1, 'religious popularity': 1, 'past years': 1, 'islamic state': 1, 'islamic revolution': 2, 'years ago': 1, 'come back': 1, 'would like': 1, 'religious popularity': 2},
+        'German': {'elite': 1.5, 'lügenpresse': 2, 'volk': 2, 'korrupt': 1.5, 'brüder schwestern': 1.5, 'islamische welt': 1, 'liebe freunde': 1, 'vergangene jahre': 1, 'islamische revolution': 1.5, 'verrat bevölkerung': 2, 'verrat volk':2,'wahrheit unterdrückt': 2, 'regierung versagt': 2, 'frau präsidentin': 1, 'direkte demokratie': 1.5},
+        'French': {'élite': 1.5, 'médias traditionnels': 2, 'peuple': 1, 'corrompu': 1.5, 'discours lalbisgüetli': 1, 'mesdames messieurs': 1, 'présidente confédération': 1, 'démocratie directe': 1.5, 'madame présidente': 1, 'conseil fédéral': 2, 'droits lhomme': 1, 'citoyennes citoyens': 1, 'libre circulation': 1, 'manière dutiliser': 1},
+    }
 
+    def is_populist(text, language):
+        score = 0
+        for term, weight in populist_terms[language].items():
+            if term in text:
+                score += weight
+        if score >= 5:  # Setze diesen Schwellenwert auf den gewünschten Wert
+            return 1
+        return 0
+
+    df['Populism'] = df.apply(lambda row: is_populist(row['Text'], row['Language']), axis=1)
 
     # Speichere den bereinigten Datensatz
     df.to_csv('data/processed/processed.csv', index=False)
